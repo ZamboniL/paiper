5750036bf3fc18430ad6c644435da90321ea6711
zambonil <lucaszam0@gmail.com>
2024-01-06 18:56:53 -0300

    Create Partners

---
+++ src/components/Partners.tsx
@@ -0,0 +1,21 @@
+export default function Partners() {
+  return (
+    <div className="mx-auto mt-20 flex flex-col gap-8">
+      <h3 className="max-w-3xl text-center text-base">As melhores empresas acreditam na Paiper</h3>
+      <ul className="flex items-center gap-12">
+        <li>
+          <img src="/img/bmp.png" alt="BMP" />
+        </li>
+        <li>
+          <img src="/img/rodobens.png" alt="Rodobens" />
+        </li>
+        <li>
+          <img src="/img/plenum.png" alt="Plenum" />
+        </li>
+        <li>
+          <img src="/img/btg.png" alt="BTG" />
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Poppins } from 'next/font/google';
 import Header from 'src/components/Header';
 import Hero from 'src/components/Hero';
+import Partners from 'src/components/Partners';
 
 const poppins = Poppins({ weight: ['400', '600'], subsets: ['latin'] });
 
@@ -9,6 +10,7 @@ export default function Home() {
     <main className={`flex h-[2400px] min-h-full flex-col ${poppins.className}`}>
       <Header />
       <Hero />
+      <Partners />
     </main>
   );
 }
