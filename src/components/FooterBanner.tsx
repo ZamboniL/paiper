1bb0f1e7ad85c8adea0dbbc2e1495ef32296c7df
zambonil <lucaszam0@gmail.com>
2024-01-21 11:51:11 -0300

    Animate footer banner

--- src/components/FooterBanner.tsx
+++ src/components/FooterBanner.tsx
@@ -1,24 +1,74 @@
+import { useScroll, useTransform } from 'framer-motion';
 import Arrow from './icons/Arrow';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+function BannerItem({
+  isHovered,
+  onMouseEnter,
+  onMouseLeave
+}: {
+  isHovered: boolean;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
+}) {
+  return (
+    <div
+      className="flex cursor-pointer items-center gap-4"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <span className="text-nowrap text-4xl font-semibold text-white md:text-5xl">
+        Veja o Paiper em ação
+      </span>
+      <motion.div
+        className="w-fit rounded-full bg-white p-2 md:p-4"
+        transition={{ ease: 'easeOut' }}
+        animate={{ rotate: isHovered ? 45 : 0 }}
+      >
+        <Arrow />
+      </motion.div>
+    </div>
+  );
+}
 
 export default function FooterBanner() {
+  const [hover, setHover] = useState<number | boolean>(false);
+  const { scrollYProgress } = useScroll();
+  const translateY = useTransform(scrollYProgress, [0, 1], [700, -200]);
+
+  const handleHover = (index: number) => () => {
+    setHover(index);
+  };
+
+  const handleHoverEnd = () => {
+    setHover(false);
+  };
+
   return (
-    <div className="flex h-24 w-full gap-16 overflow-x-hidden bg-primary-400 py-12 md:py-16">
-      <div className="flex items-center gap-4">
-        <span className="text-nowrap text-4xl font-semibold text-white md:text-5xl">
-          Veja o Paiper em ação
-        </span>
-        <div className="w-fit rounded-full bg-white p-2 md:p-4">
-          <Arrow />
-        </div>
-      </div>
-      <div className="flex items-center gap-4">
-        <span className="text-nowrap text-4xl font-semibold text-white md:text-5xl">
-          Veja o Paiper em ação
-        </span>
-        <div className="w-fit rounded-full bg-white p-2 md:p-4">
-          <Arrow />
-        </div>
-      </div>
+    <div className="relative w-full overflow-hidden bg-primary-400">
+      <motion.div style={{ translateX: translateY }} className=" flex h-24 gap-16 py-12 md:py-16">
+        <BannerItem
+          isHovered={hover === 1}
+          onMouseEnter={handleHover(1)}
+          onMouseLeave={handleHoverEnd}
+        />
+        <BannerItem
+          isHovered={hover === 2}
+          onMouseEnter={handleHover(2)}
+          onMouseLeave={handleHoverEnd}
+        />
+        <BannerItem
+          isHovered={hover === 3}
+          onMouseEnter={handleHover(3)}
+          onMouseLeave={handleHoverEnd}
+        />
+        <BannerItem
+          isHovered={hover === 4}
+          onMouseEnter={handleHover(4)}
+          onMouseLeave={handleHoverEnd}
+        />
+      </motion.div>
     </div>
   );
 }
