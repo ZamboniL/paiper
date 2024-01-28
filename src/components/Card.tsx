3b654ee291f8746fde25f146ac01e819888580c0
zambonil <lucaszam0@gmail.com>
2024-01-28 15:36:33 -0300

    Animate cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,59 @@
+import { Variants, motion } from 'framer-motion';
+
+const cardVariants: Variants = {
+  rest: {
+    background: '#F8F8FA'
+  },
+  hover: {
+    background: '#2351FF',
+    color: 'white'
+  }
+};
+
+const iconBoxVariants: Variants = {
+  rest: {
+    scale: 1.2,
+    background: '#2351FF',
+    boxShadow: '0px 0px 20px rgba(35, 81, 255, 0.2), 0px 0px 8px #6EA3FF inset',
+    stroke: '#ffffff',
+    fill: '#ffffff'
+  },
+  hover: {
+    scale: 1,
+    background: '#ffffff',
+    boxShadow: 'none',
+    stroke: '#2351FF',
+    fill: '#2351FF'
+  }
+};
+
+const tagVariants: Variants = {
+  rest: {
+    color: '#2351FF'
+  },
+  hover: {
+    color: '#ffffff'
+  }
+};
+
+const titleVariants: Variants = {
+  rest: {
+    color: '#132053'
+  },
+  hover: {
+    color: '#ffffff'
+  }
+};
+
+const textVariants: Variants = {
+  rest: {
+    color: '#7A8089'
+  },
+  hover: {
+    color: '#ffffff'
+  }
+};
+
 interface CardProps {
   className?: string;
   Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
@@ -8,31 +64,44 @@ interface CardProps {
 
 export default function Card({ title, tag, description, Icon, className }: CardProps) {
   return (
-    <div
+    <motion.div
+      initial="rest"
+      whileHover="hover"
+      animate="rest"
+      variants={cardVariants}
+      transition={{
+        duration: 0.3,
+        ease: 'easeInOut'
+      }}
       className={`flex w-full flex-col justify-between rounded-lg border-[1px]
   border-primary-100 bg-primary-50 p-8 ${className}`}
     >
       {Icon && (
-        <div
-          className="mb-11 flex h-14 w-14 items-center justify-center rounded-lg bg-primary-400
-       shadow-[0_0_20px_0_rgba(35,81,255,0.20),_0_0_8px_0_#6EA3FF_inset]"
+        <motion.div
+          variants={iconBoxVariants}
+          className="mb-11 flex h-14 w-14 items-center justify-center rounded-lg"
         >
           <Icon className="h-8 w-8" />
-        </div>
+        </motion.div>
       )}
-      {tag && <span className="mb-8 text-[40px] font-semibold text-primary-400">{tag}</span>}
+      {tag && (
+        <motion.span variants={tagVariants} className="mb-8 text-[40px] font-semibold">
+          {tag}
+        </motion.span>
+      )}
       <div>
         {title && (
-          <h3
+          <motion.h3
+            variants={titleVariants}
             className={`${
               Icon ? 'pb-2' : 'pb-5'
             } flex flex-row items-center gap-4 pb-2 text-base font-semibold`}
           >
             {title}
-          </h3>
+          </motion.h3>
         )}
-        <p className="text-primary-200">{description}</p>
+        <motion.p variants={textVariants}>{description}</motion.p>
       </div>
-    </div>
+    </motion.div>
   );
 }
